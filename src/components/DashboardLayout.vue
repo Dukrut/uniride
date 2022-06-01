b85da521d4eb9167748f1e89eb351b38739aefd4
Eduardo Krutzsch <eduardo-krutzsch@hotmail.com>
2022-06-01 20:51:16 -0300

    Desenvolvido layout do dashboard

---
+++ src/components/DashboardLayout.vue
@@ -0,0 +1,249 @@
+<template>
+  <div class="full-width">
+    <div class="row justify-between q-pa-lg">
+      <div class="col-3">
+        <q-card>
+          <div class="text-center q-pa-sm">
+            <span><b>Pessoas com mais carros</b></span>
+          </div>
+          <div class="box-graph">
+            <table class="charts-css column datasets-spacing-4 chart">
+              <tbody>
+                <tr>
+                  <td>Camille</td>
+                </tr>
+                <tr>
+                  <td>Thiago</td>
+                </tr>
+                <tr>
+                  <td>Hayana</td>
+                </tr>
+                <tr>
+                  <td>Eduardo</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </q-card>
+      </div>
+      <div class="col-3">
+        <q-card>
+          <div class="text-center q-pa-sm">
+            <span><b>Pessoas com que pegaram mais caronas</b></span>
+          </div>
+          <div class="box-graph">
+            <table class="charts-css column datasets-spacing-4 chart">
+              <tbody>
+                <tr>
+                  <td>Camille</td>
+                </tr>
+                <tr>
+                  <td>Thiago</td>
+                </tr>
+                <tr>
+                  <td>Hayana</td>
+                </tr>
+                <tr>
+                  <td>Eduardo</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </q-card>
+      </div>
+      <div class="col-3">
+        <q-card>
+          <div class="text-center q-pa-sm">
+            <span><b>Pessoas que deram mais caronas</b></span>
+          </div>
+          <div class="box-graph">
+            <table class="charts-css column datasets-spacing-4 chart">
+              <tbody>
+                <tr>
+                  <td>Camille</td>
+                </tr>
+                <tr>
+                  <td>Thiago</td>
+                </tr>
+                <tr>
+                  <td>Hayana</td>
+                </tr>
+                <tr>
+                  <td>Eduardo</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </q-card>
+      </div>
+    </div>
+
+    <div class="row justify-between q-pa-lg">
+      <div class="col-3">
+        <q-card>
+          <div class="text-center q-pa-sm">
+            <span><b>Avaliação média por corrida </b></span>
+          </div>
+          <div class="box-graph text-center">
+            <span class="material-icons star-rate"> star_rate </span>
+            <br />
+            <span class="ratting-text"><b>4.5</b></span>
+          </div>
+        </q-card>
+      </div>
+      <div class="col-3">
+        <q-card>
+          <div class="text-center q-pa-sm">
+            <span><b>Pessoas com mais carros</b></span>
+          </div>
+          <div class="box-graph">
+            <table class="charts-css column datasets-spacing-4 chart">
+              <tbody>
+                <tr>
+                  <td>Camille</td>
+                </tr>
+                <tr>
+                  <td>Thiago</td>
+                </tr>
+                <tr>
+                  <td>Hayana</td>
+                </tr>
+                <tr>
+                  <td>Eduardo</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </q-card>
+      </div>
+      <div class="col-3">
+        <q-card>
+          <div class="text-center q-pa-sm">
+            <span><b>Pessoas com mais carros</b></span>
+          </div>
+          <div class="box-graph">
+            <table class="charts-css column datasets-spacing-4 chart">
+              <tbody>
+                <tr>
+                  <td>Camille</td>
+                </tr>
+                <tr>
+                  <td>Thiago</td>
+                </tr>
+                <tr>
+                  <td>Hayana</td>
+                </tr>
+                <tr>
+                  <td>Eduardo</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </q-card>
+      </div>
+    </div>
+
+    <div class="row justify-between q-pa-lg">
+      <div class="col-3">
+        <q-card>
+          <div class="text-center q-pa-sm">
+            <span><b>Pessoas com mais carros</b></span>
+          </div>
+          <div class="box-graph">
+            <table class="charts-css column datasets-spacing-4 chart">
+              <tbody>
+                <tr>
+                  <td>Camille</td>
+                </tr>
+                <tr>
+                  <td>Thiago</td>
+                </tr>
+                <tr>
+                  <td>Hayana</td>
+                </tr>
+                <tr>
+                  <td>Eduardo</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </q-card>
+      </div>
+      <div class="col-3">
+        <q-card>
+          <div class="text-center q-pa-sm">
+            <span><b>Pessoas com mais carros</b></span>
+          </div>
+          <div class="box-graph">
+            <table class="charts-css column datasets-spacing-4 chart">
+              <tbody>
+                <tr>
+                  <td>Camille</td>
+                </tr>
+                <tr>
+                  <td>Thiago</td>
+                </tr>
+                <tr>
+                  <td>Hayana</td>
+                </tr>
+                <tr>
+                  <td>Eduardo</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </q-card>
+      </div>
+      <div class="col-3">
+        <q-card>
+          <div class="text-center q-pa-sm">
+            <span><b>Pessoas com mais carros</b></span>
+          </div>
+          <div class="box-graph">
+            <table class="charts-css column datasets-spacing-4 chart">
+              <tbody>
+                <tr>
+                  <td>Camille</td>
+                </tr>
+                <tr>
+                  <td>Thiago</td>
+                </tr>
+                <tr>
+                  <td>Hayana</td>
+                </tr>
+                <tr>
+                  <td>Eduardo</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </q-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "charts.css";
+export default {
+  mounted() {
+    console.log("teste");
+  },
+};
+</script>
+
+<style>
+.box-graph {
+  height: 170px !important;
+  padding: 10px;
+}
+
+.star-rate {
+  font-size: 100px;
+  color: rgba(255, 220, 90, 0.75);
+}
+
+.ratting-text {
+  font-size: 35px;
+}
+</style>
